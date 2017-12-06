769b0d00dad0837187fbfd4220bbd872dbd2c54f
Glitch (torch-nitrogen) <none>
2017-12-06 07:12:41 +0000

    null

--- routes/users.js
+++ routes/users.js
@@ -16,63 +16,28 @@ router.use(cookieSession({
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 //Register User
-router.post('/register',(req,res)=>{
-  console.log("registration attempt "+Account)
- Account.findOne({username: req.body.username}, (err,result)=>{
-   if(!result){
-     new Account ({
-       username: req.body.username,
-       password: req.body.password
-     }).save().then(()=>{
-       res.send('registration complete')
-     })
-   }
- })
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
 
-})
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
 router.use(passport.initialize());
 router.use(passport.session());
 
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    Account.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false);
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-  }
-));
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
-router.post('/login',
-  passport.authenticate('local', {
-     successRedirect: '/loginSuccess',
-    failureRedirect: '/loginFailure'
-  })
-);
-
-passport.serializeUser(function(user, done) {
- console.log("serializing")
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  console.log("serializing")
-  done(null, user);
-});
-
-
-router.get('/loginFailure', function(req, res, next) {
-  res.send('Failed to authenticate');
-});
-
-router.get('/loginSuccess', function(req, res, next) {
-  res.send('Successfully authenticated');
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
 });
 
 module.exports = router;
