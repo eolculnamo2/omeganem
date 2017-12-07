9f30caacd1a785f5529be9ef506db56215d0e2c6
Glitch (torch-nitrogen) <none>
2017-12-07 06:48:08 +0000

    Styling and Responsive

--- public/client.js
+++ public/client.js
@@ -2,3 +2,13 @@
 function move(x){
   document.getElementById(x).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
+
+window.addEventListener('resize',()=>{
+  var w = window.innerWidth
+  if(w < 1170){
+    document.getElementById("title").style.display="none";
+  }
+  else if (w >= 1170){
+    document.getElementById("title").style.display="inline";
+  }
+})
